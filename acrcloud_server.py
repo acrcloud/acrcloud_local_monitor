c08df6e5c19a667afaf386f3148f7417ba5d3184
ICYMYM <573230586@qq.com>
2016-03-29 12:37:28 +0800

    bug1

--- acrcloud_server.py
+++ acrcloud_server.py
@@ -11,7 +11,7 @@ import sys
 import traceback
 from acrcloud_config import config
 from twisted.internet import reactor
-from acrcloud_manager import acrcloudManager
+from acrcloud_manager import AcrcloudManager
 from twisted.internet.protocol import Protocol, ServerFactory
 
 reload(sys)
